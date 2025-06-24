d6c563d19ae4192748001038400e192788de20f0
lagomez <lagomez@juslapampa.gob.ar>
2025-06-24 08:40:18 -0300

    colapseList

--- src/app/services/to-do.service.ts
+++ src/app/services/to-do.service.ts
@@ -6,6 +6,9 @@ const todoDefault : Tarea[] = [
       {id: 0,titulo:"Hacer tarea1",fechaCreacion:new Date(), descripcion:"hacer tarea de math", estado: 'pendiente'},
       {id: 2,titulo:"Hacer tarea2",fechaCreacion:new Date(), descripcion:"hacer tarea de X", estado: 'en progreso'},
       {id: 3,titulo:"Hacer tarea3",fechaCreacion:new Date(), descripcion:"hacer tarea de Y", estado: 'completada'},
+      {id: 0,titulo:"Hacer tarea1",fechaCreacion:new Date(), descripcion:"hacer tarea de math", estado: 'pendiente'},
+      {id: 2,titulo:"Hacer tarea2",fechaCreacion:new Date(), descripcion:"hacer tarea de X", estado: 'en progreso'},
+      {id: 3,titulo:"Hacer tarea3",fechaCreacion:new Date(), descripcion:"hacer tarea de Y", estado: 'completada'},
     ]
 
 @Injectable({
