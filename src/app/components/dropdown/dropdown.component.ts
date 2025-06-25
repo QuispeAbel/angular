bfb3c16f472b3b68f985c02c97da5b76d2416ff4
QuispeAbel <quisperobledoabel@gmail.com>
2025-06-24 21:46:19 -0300

    DropDownSi

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { formOptions } from 'src/app/config/options';
 
 @Component({
   selector: 'app-dropdown',
@@ -12,21 +11,11 @@ export class DropdownComponent {
   @Input() estado?: any;
   @Output() onSelect: EventEmitter<any> = new EventEmitter<any>();
 
-  //public estado : string = '';
-
-  public open: boolean = false;
-
-  public toggleDropdown() {
-    this.open = !this.open;
-  }
-
   public selectOption(option: any) {
     this.onSelect.emit(option);
-    this.open = false;
   }
 
   mostrar(){
-    console.log("estado en DD"+this.estado)
     this.onSelect.emit(this.estado);
   }
 }
