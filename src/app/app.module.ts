61b9a573d27b4c13460e8db7d947342ddbd485f0
lagomez <lagomez@juslapampa.gob.ar>
2025-06-27 11:37:42 -0300

    guardDetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,7 +43,7 @@ const routes: Routes = [
   },{
     path: 'dollar/:casa',
     component: DetailsDollarComponent,
-    // canActivate: [AuthGuard]
+    canActivate: [AuthGuard]
   },
   {
     path:'**',
